d7a70beae031517f6899d30d6e99025b4f52c305
towhidulbu08 <towhidulislam26121997@gmail.com>
2026-07-22 02:43:27 +0600

    Configuring Environment Variables With dotenv And Using Express Built In Middlwares In app.ts

--- project-prisma-press/src/app.ts
+++ project-prisma-press/src/app.ts
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.get("/", (req: Request, res: Response) => {
+app.get("/", async (req: Request, res: Response) => {
   res.send("Hello, World!");
 });
 
