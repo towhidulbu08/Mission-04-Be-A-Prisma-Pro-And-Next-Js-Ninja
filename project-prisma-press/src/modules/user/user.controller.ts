33180969e3343ccbcd0818c7c9bdb626cc1165e2
towhidulbu08 <towhidulislam26121997@gmail.com>
2026-07-22 18:36:59 +0600

    21-1 Defining Catch Async As Higher Order Function

--- project-prisma-press/src/modules/user/user.controller.ts
+++ project-prisma-press/src/modules/user/user.controller.ts
@@ -1,10 +1,35 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import httpStatus from "http-status";
+import catchAsync from "../../utils/catchAsync";
 import { userServices } from "./user.services";
 
-const registerUser = async (req: Request, res: Response) => {
-  try {
-    const userData = await userServices.registerUserIntoDB(req.body);
+// const registerUser = async (req: Request, res: Response) => {
+//   try {
+//     const userData = await userServices.registerUserIntoDB(req.body);
+
+//     res.status(httpStatus.CREATED).json({
+//       success: true,
+//       statusCode: httpStatus.CREATED,
+//       message: "User registered successfully",
+//       data: {
+//         userData,
+//       },
+//     });
+//   } catch (error) {
+//     console.log(error);
+//     res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+//       success: false,
+//       statusCode: httpStatus.INTERNAL_SERVER_ERROR,
+//       message: "Failed to register user",
+//       error: (error as Error).message,
+//     });
+//   }
+// };
+
+const registerUser = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const payload = req.body;
+    const userData = await userServices.registerUserIntoDB(payload);
 
     res.status(httpStatus.CREATED).json({
       success: true,
@@ -14,17 +39,8 @@ const registerUser = async (req: Request, res: Response) => {
         userData,
       },
     });
-  } catch (error) {
-    console.log(error);
-    res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
-      success: false,
-      statusCode: httpStatus.INTERNAL_SERVER_ERROR,
-      message: "Failed to register user",
-      error: (error as Error).message,
-    });
-  }
-};
-
+  },
+);
 export const userController = {
   registerUser,
 };
